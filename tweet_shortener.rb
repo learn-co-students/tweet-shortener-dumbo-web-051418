0800d27221024ea11c68686a71d40ad00da213da
Eric Razor <eric.razorjr@gmail.com>
2018-05-08 12:26:17 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def dictionary
+  words = {
+    "hello" => 'hi',
+    "to" => 2,
+    "two" => 2,
+    "too" => 2,
+    "for" => 4,
+    "four" => 4,
+    "be" => 'b',
+    "you" => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(str)
+  str.split(" ").map do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end
+end
